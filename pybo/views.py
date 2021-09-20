6c864a700acb725147d0d26aa2cdb3c82163240d
psw09 <tjddns09@gmail.com>
2021-09-20 23:02:39 +0900

    insert 수정

--- pybo/views.py
+++ pybo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 import my_settings
 from .models import NewsInfo
@@ -47,27 +47,12 @@ def newsInsert(request):
 
 
 def newsInsertVal(request):
-    # conn = pymysql.connect(host='localhost', user='root', password='root', db='news_db', charset='utf8')
-    # curs = conn.cursor()
-    #
-    # sql = """
-    #         SELECT NEWS_TITLE, NEWS_CONTENT, NEWS_DTTM, NEWS_EDITOR, MEDIA_OUTLETS
-    #         FROM news_db.news_info
-    #       """
-    # curs.execute(sql)
-    # rows = curs.fetchall()
-    # print(rows)
-    # conn.close()
     if request.method == "POST":
        newsTitle = request.POST.get("newsTitle");
        newsContent = request.POST.get("newsContent");
        newsEditor = request.POST.get("newsEditor");
        mediaOutlets = request.POST.get("mediaOutlets");
-    print("@@@@@");
-    print(newsTitle);
-    print(newsContent);
-    print(newsEditor);
-    print(mediaOutlets);
+
 
     conn = pymysql.connect(host='localhost', user='root', password='root', db='news_db', charset='utf8')
     curs = conn.cursor()
@@ -80,15 +65,13 @@ def newsInsertVal(request):
                                   MEDIA_OUTLETS)
                                  VALUES('{newsTitle}',
                                       '{newsContent}',
-                                      SYSDATE,
+                                      now(),
                                       '{newsEditor}',
                                       '{mediaOutlets}')
           """.format(newsTitle=newsTitle, newsContent=newsContent,newsEditor=newsEditor,mediaOutlets=mediaOutlets)
     curs.execute(sql)
-    print("**********************")
-    print(sql)
-    rows = curs.fetchall()
-    print(rows)
+
+    conn.commit()
+
     conn.close()
-    newsList = {'newsList': rows}
-    return render(request, 'pybo/newsInsert.html', newsList)
+    return redirect('/pybo')
